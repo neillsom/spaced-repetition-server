cfccf095f3c051b130d5b93128851eb8cdfd1210
neillsom <neillsom@mail.com>
2018-07-20 16:17:49 -0700

    question model/schema

---
+++ models/questions.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+	prompt: { type: String, required: true },
+	answer: { type: String, required: true }
+});
+
+questionSchema.index({ prompt: 1, answer: 1 }, { unique: true });
+
+questionSchema.set('toObject', {
+	transform: function (doc, ret) {
+		ret.id = ret._id;
+		delete ret._id;
+		delete ret.__v;
+	}
+});
+
+module.exports = mongoose.model('Question', questionSchema);
