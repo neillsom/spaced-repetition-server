17ac887f2976fe33cb6d820a20f42e0418aef593
neillsom <neillsom@mail.com>
2018-07-19 18:37:46 -0700

    added mocha and chai template

---
+++ test/setup.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { TEST_DATABASE_URL } = require('../config');
+const { dbConnect, dbDisconnect } = require('../db-mongoose');
+
+process.env.NODE_ENV = 'test';
+
+// Clear the console before each run
+process.stdout.write('\x1Bc\n');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+before(function() {
+	return dbConnect(TEST_DATABASE_URL);
+});
+
+after(function() {
+	return dbDisconnect();
+});
+
+describe('Mocha and Chai', function() {
+	it('should be properly setup', function() {
+		expect(true).to.be.true;
+	});
+});
