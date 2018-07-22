24540cf7f2788c87b654266a1b7bd4f60c257057
neillsom <neillsom@mail.com>
2018-07-22 01:54:18 -0700

    added body-parser to packages for use in auth login

--- users/routes/auth.js
+++ users/routes/auth.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const User = require('../../users/user');
@@ -10,6 +11,8 @@ const { JWT_SECRET, JWT_EXPIRY } = require('../../config');
 const router = express.Router();
 
 const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+
+router.use(bodyParser.json());
 
 router.post('/login', localAuth, (req, res) => {
 	const authToken = createAuthToken(req.user);
